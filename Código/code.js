efcfa65a26eef3f4df52020ed1c1baae6bc70b31
VitorEduardo26 <vitor.eduardo2611@hotmail.com>
2021-08-29 21:11:37 -0300

    0.2
    Realizei as declarações de variáveis e criei botões.

--- Código/code.js
+++ Código/code.js
@@ -1,4 +1,21 @@
 "use strict"
 
 //Variáveis
-var 
+var hora = 0;
+var minuto = 0;
+var segundo = 0;
+
+var tempo = 1000; //Quantos milésimos vale um segundo.
+var cronometro;
+
+function iniciar(){
+
+}
+
+function reiniciar(){
+
+}
+
+function pausar(){
+
+}
