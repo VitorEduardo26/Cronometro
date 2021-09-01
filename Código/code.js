361d7e2116fa3c0721dbb797eb38b0090721f996
VitorEduardo26 <vitor.eduardo2611@hotmail.com>
2021-09-01 02:24:16 -0300

    1.0
    Código básico concluído (fase de melhorias em aberto). HTML em 50%.

--- Código/code.js
+++ Código/code.js
@@ -1,21 +1,71 @@
 "use strict"
 
 //Variáveis
-var hora = 0;
+var hora = 1;
 var minuto = 0;
 var segundo = 0;
 
+let h;
+let m;
+let s;
+
 var tempo = 1000; //Quantos milésimos vale um segundo.
-var cronometro;
+var cronometro;//Seleciona o intervalo.
 
-function iniciar(){
 
-}
 
-function reiniciar(){
 
-}
+//Inicia a contagem.
+function iniciar() {
+    var a;
+    cronometro = setInterval(contar, tempo);
+    a = document.getElementById("pausa").innerHTML = '<button onclick="pausar()">Pausar</button>';
+};
 
-function pausar(){
 
-}
+//Paro a contagem.
+function pausar() {
+    clearInterval(cronometro);
+    var a;
+    a = document.getElementById("pausa").innerHTML = '<button onclick="iniciar()">Continuar</button>';
+};
+
+
+//Reinicia a contagem.
+function reiniciar() {
+    //Retorno os valores todos aos iniciais e pauso a contagem.
+    pausar();
+    hora = "0" + 1;
+    minuto = "0" + 0;
+    segundo = "0" + 0;
+
+    s = document.getElementById("segundos").innerHTML = segundo;
+    m = document.getElementById("minutos").innerHTML = minuto;
+    h = document.getElementById("horas").innerHTML = hora
+
+    var a;
+    a = document.getElementById("pausa").innerHTML = '<button onclick="iniciar()">Iniciar</button>';
+};
+
+
+//Contador.
+function contar() {
+
+    //Pego cada elemento e a cada "tempo" adiciono um, faço as verificações até dez e até 60.
+
+    s = document.getElementById("segundos").innerHTML = segundo < 10 ? "0" + segundo++ : segundo++;
+
+    if (segundo == 60) {
+        segundo = 0;
+        m = document.getElementById("minutos").innerHTML = minuto < 10 ? "0" + minuto++ : minuto++;
+
+    };
+
+    if (minuto == 60) {
+        minuto = 0;
+        h = document.getElementById("horas").innerHTML = hora < 10 ? "0" + hora++ : hora++;
+
+    };
+    //Verificação até 10: concateno um 0 em números com apenas uma casa.
+    //Verificação até 60: a cada 60 segundos 1 minuto e assim com minutos também.
+};
